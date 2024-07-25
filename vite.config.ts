b730819d2e5ae21e910ebca0885a4de3b60e0d4e
Adebowale Akinola <adebowalen@yahoo.com>
2024-07-25 22:09:42 +0200

    configure unit test

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(),],
-})
+  plugins: [react(), tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setupTests.ts',
+    coverage: {
+      thresholds: {
+        lines: 70,
+        branches: 70,
+        functions: 70,
+        statements: 70,
+      },
+      exclude: ['.dev_tools', 'src/test', 'src/vite-env.d.ts', '*.cjs'],
+    },
+    exclude: ['node_modules', '.dev_tools', 'src/test'],
+  },
+});
